fcd554253c377e0895e33282c908f2343c142308
German Hernandez <muygrafico@gmail.com>
2016-11-20 23:57:47 -0500

    wip 7

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,42 @@
 (function () {
+  var evaluationLinks = {
+    'claro': 'https://karisma.org.co/descargar/donde-estan-mis-datos-informe-2016-claro/',
+    'telefonica': 'https://karisma.org.co/descargar/donde-estan-mis-datos-informe-2016-telefonica-movistar/',
+    'tigoUne': 'https://karisma.org.co/descargar/donde-estan-mis-datos-informe-2016-tigo-une/',
+    'etb': 'https://karisma.org.co/descargar/donde-estan-mis-datos-informe-2016-tigo-une/',
+    'directv': 'https://karisma.org.co/descargar/donde-estan-mis-datos-informe-2016-directv/'
+  }
+
   function handleActiveTab (company) {
     var elems = document.querySelector('.active')
     if (elems !== null) { elems.classList.remove('active') }
     var d = document.getElementsByClassName(company)[1]
     d.className += ' active'
+  }
+
+  function updateLink (element, link) {
+    var el = document.getElementsByClassName(element)[0]
+    el.href = link
+  }
+
+  function handleActiveEvaluationLink (company) {
+    switch (company) {
+      case 'claro':
+        updateLink('company-evaluation-link', evaluationLinks.claro)
+        break
+      case 'telefonica':
+        updateLink('company-evaluation-link', evaluationLinks.telefonica)
+        break
+      case 'tigo-une':
+        updateLink('company-evaluation-link', evaluationLinks.tigoUne)
+        break
+      case 'etb':
+        updateLink('company-evaluation-link', evaluationLinks.etb)
+        break
+      case 'directv':
+        updateLink('company-evaluation-link', evaluationLinks.directv)
+        break
+    }
   }
 
   function updateEvaluation (event) {
@@ -12,6 +45,7 @@
       .style.backgroundImage = "url('/img/evaluation/" + company + ".png')"
 
     handleActiveTab(company)
+    handleActiveEvaluationLink(company)
   }
 
   var claros = Array.from(document.getElementsByClassName('claro'))
