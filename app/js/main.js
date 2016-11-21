55c36e1d876282d51d1374c2deab234522153da4
German Hernandez <muygrafico@gmail.com>
2016-11-20 22:08:19 -0500

    wip 7

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,37 @@
 (function () {
-  function f1 (event) {
+  function updateEvaluation (event) {
     console.log(event.target)
 
     var company = event.target.getAttribute('company')
     document.getElementsByClassName('evaluation-info')[0]
       .style.backgroundImage = "url('/img/evaluation/" + company + ".png')"
-
-    document.getElementsByClassName('active')[1].className = ''
   }
 
   var claros = Array.from(document.getElementsByClassName('claro'))
 
   claros.forEach(function (element) {
-    element.addEventListener('click', f1)
+    element.addEventListener('click', updateEvaluation)
   })
 
   var telefonicas = Array.from(document.getElementsByClassName('telefonica'))
 
   telefonicas.forEach(function (element) {
-    element.addEventListener('click', f1)
+    element.addEventListener('click', updateEvaluation)
   })
 
   var tigoUnes = Array.from(document.getElementsByClassName('tigo-une'))
 
   tigoUnes.forEach(function (element) {
-    element.addEventListener('click', f1)
+    element.addEventListener('click', updateEvaluation)
   })
 
   var etbs = Array.from(document.getElementsByClassName('etb'))
   etbs.forEach(function (element) {
-    element.addEventListener('click', f1)
+    element.addEventListener('click', updateEvaluation)
   })
 
   var directvs = Array.from(document.getElementsByClassName('directv'))
   directvs.forEach(function (element) {
-    element.addEventListener('click', f1)
+    element.addEventListener('click', updateEvaluation)
   })
 })()
